374eef35556e3fd4f448ffd45e3a182c0e822581
york-fu <york-fu@outlook.com>
2025-08-17 14:42:57 +0800

    init: add files

---
+++ launch/display.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+import os
+
+
+def generate_launch_description():
+    pkg_share = FindPackageShare("robot_description_template").find("robot_description_template")
+    urdf_file = os.path.join(pkg_share, "urdf", "robot_description_template.urdf")
+    rviz_config = os.path.join(pkg_share, "rviz", "display.rviz")
+
+    return LaunchDescription([
+        Node(
+            package="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui",
+            name="joint_state_publisher_gui",
+            output="screen"
+        ),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            output="screen",
+            parameters=[{"robot_description": open(urdf_file).read()}]
+        ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            arguments=["-d", rviz_config],
+            output="screen"
+        )
+    ])
---
+++ launch/gazebo.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+import os
+
+
+def generate_launch_description():
+    pkg_share = FindPackageShare("robot_description_template").find("robot_description_template")
+    urdf_file = PathJoinSubstitution([pkg_share, "urdf", "robot_description_template.urdf"])
+    urdf_file = os.path.join(pkg_share, "urdf", "robot_description_template.urdf")
+
+    gz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(
+                FindPackageShare("ros_gz_sim").find("ros_gz_sim"),
+                "launch",
+                "gz_sim.launch.py"
+            )]
+        ),
+        launch_arguments={"gz_args": "-r empty.sdf"}.items()
+    )
+
+    robot_state_pub = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": open(urdf_file).read()}],
+        output="screen"
+    )
+
+    spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=["-name", "ncb_a1_description", "-topic", "robot_description", "-z", "1.2"],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        gz_launch,
+        robot_state_pub,
+        spawn_robot
+    ])
